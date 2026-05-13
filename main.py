7a77a752dfd5d7e53f25a5657631b34431cb504f
jonathanrodrigues15287 <jonathanrodrigues15287@gmail.com>
2026-05-13 21:35:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
-
 from ui.dashboard import Homescreen
+from kivy.core.window import Window
+
+Window.size = (360, 640)
 
 class MyApp(App):
     def build(self):
