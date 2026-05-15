374275a3a29f3a38a470c0d4692b5d7512aab5e8
jonathanrodrigues15287 <jonathanrodrigues15287@gmail.com>
2026-05-15 11:31:53 +0530

    Create budget_manager.py

---
+++ core/budget_manager.py
@@ -0,0 +1,18 @@
+from core.csv_handler import read_csv
+
+BUDGET_FILE = "data/budgets.csv"
+EXPENSES_FILE = "data/expenses.csv"
+
+
+def calculate_balance():
+
+    budget_df = read_csv(BUDGET_FILE)
+    expenses_df = read_csv(EXPENSES_FILE)
+
+    latest_budget = budget_df.iloc[-1]["Budget"]
+
+    total_expenses = expenses_df["Amount"].sum()
+
+    balance = latest_budget - total_expenses
+
+    return balance
