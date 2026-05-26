3eda9db5b076bae4ee6522372738df4af4f4ca15
jonathanrodrigues15287 <jonathanrodrigues15287@gmail.com>
2026-05-26 11:42:39 +0530

    Update budget_manager.py

--- core/budget_manager.py
+++ core/budget_manager.py
@@ -9,7 +9,10 @@ def calculate_balance():
     budget_df = read_csv(BUDGET_FILE)
     expenses_df = read_csv(EXPENSES_FILE)
 
-    latest_budget = budget_df.iloc[-1]["Budget"]
+    if budget_df.empty:
+        latest_budget = 0.0
+    else:
+        latest_budget = budget_df.iloc[-1]["Budget"]
 
     total_expenses = expenses_df["Amount"].sum()
 
