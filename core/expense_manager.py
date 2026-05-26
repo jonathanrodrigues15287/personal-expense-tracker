f6a36a8ab71d787ee7d7bcb4c01be8f52e33cdaa
jonathanrodrigues15287 <jonathanrodrigues15287@gmail.com>
2026-05-26 16:13:51 +0530

    Update expense_manager.py

--- core/expense_manager.py
+++ core/expense_manager.py
@@ -18,45 +18,30 @@ def add_expense(expense_id,
     }
 
     append_expense(expense_data)
-
     print("Expense Added Successfully")
 
 def view_all_expenses():
-
     df = read_csv(EXPENSES_FILE)
-
     return df
 
 def calculate_total_expenses():
-
     df = read_csv(EXPENSES_FILE)
-
     return df["Amount"].sum()
 
 def category_expenses():
-
     df = read_csv(EXPENSES_FILE)
-
     category_totals = df.groupby("Category")["Amount"].sum()
-
     return category_totals
 
 def daily_expenses():
-
     df = read_csv(EXPENSES_FILE)
-
     daily_totals = df.groupby("Date")["Amount"].sum()
-
     return daily_totals
 
 def delete_expense(expense_id):
-
     df = read_csv(EXPENSES_FILE)
-
     df = df[df["Expense_ID"] != expense_id]
-
     df.to_csv(EXPENSES_FILE, index=False)
-
     print("Expense Deleted Successfully")
 
 def edit_expense(expense_id,
@@ -66,9 +51,7 @@ def edit_expense(expense_id,
                  new_description=None):
 
     df = read_csv(EXPENSES_FILE)
-
     index = df[df["Expense_ID"] == expense_id].index
-
     if len(index) == 0:
         print("Expense ID Not Found")
         return
@@ -92,17 +75,11 @@ def edit_expense(expense_id,
     print("Expense Updated Successfully")
 
 def filter_by_category(category):
-
     df = read_csv(EXPENSES_FILE)
-
     filtered_df = df[df["Category"] == category]
-
     return filtered_df
 
 def filter_by_date(date):
-
     df = read_csv(EXPENSES_FILE)
-
     filtered_df = df[df["Date"] == date]
-
     return filtered_df
